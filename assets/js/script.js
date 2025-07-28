05b17170ea9a6b5703641d064b8d947f691b4f07
RussiSunni <jonathandyason@hotmail.com>
2025-07-28 16:33:42 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,18 +99,18 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () {
-  elementToggleFunc(this);
-});
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-  });
-}
+// select.addEventListener("click", function () {
+//   elementToggleFunc(this);
+// });
+// // add event in all select items
+// for (let i = 0; i < selectItems.length; i++) {
+//   selectItems[i].addEventListener("click", function () {
+//     let selectedValue = this.innerText.toLowerCase();
+//     selectValue.innerText = this.innerText;
+//     elementToggleFunc(select);
+//     filterFunc(selectedValue);
+//   });
+// }
 
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
